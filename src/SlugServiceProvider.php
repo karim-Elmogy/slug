529b88f2c30b332bfc7f924a71d4719280c97617
Karim Elmogy <kimo72636@gmail.com>
2024-12-10 15:16:36 +0200

    GH

--- src/SlugServiceProvider.php
+++ src/SlugServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace company\slug;
+namespace companyalexon\slug;
 
 
 use Illuminate\Support\ServiceProvider;
@@ -11,7 +11,7 @@ class SlugServiceProvider extends ServiceProvider{
 
     public function boot(){
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/../src/views', 'company/slug');
+        $this->loadViewsFrom(__DIR__.'/../src/views', 'companyalexon/slug');
     }
 
     public function register(){
